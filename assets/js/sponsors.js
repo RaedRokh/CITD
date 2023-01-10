0f4bd66da8f2f0f3d9ad372f302e8cfeec931cb6
Rayen-Allaya <rayen.alleya@supcom.tn>
2023-01-10 13:50:37 +0100

    sponsors

---
+++ assets/js/sponsors.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#line1').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+    $('#line2').slick({
+        slidesToShow: 6,
+        slidesToScroll: -1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
