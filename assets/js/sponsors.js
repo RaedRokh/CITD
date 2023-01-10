dc274619dc3e6f28d7d79dbb10d987abc44e188c
Rayen-Allaya <rayen.alleya@supcom.tn>
2023-01-10 15:16:20 +0100

    sponsor slider fixing

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     $('#line1').slick({
+        // mobileFirst:true,
+        autoplaySpeed: 1000,
+        speed : 1000,
+        // cssEase: 'linear',
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
         arrows: false,
         dots: false,
         pauseOnHover: false,
@@ -19,24 +22,27 @@ $(document).ready(function(){
             }
         }]
     });
-    $('#line2').slick({
-        slidesToShow: 6,
-        slidesToScroll: -1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
+    // $('#line2').slick({
+    //     mobileFirst:true,
+    //     slidesToScroll: -1,
+    //     cssEase: 'linear',
+    //     speed: 3000,
+    //     autoplaySpeed: 40,
+    //     slidesToShow: 4,
+    //     autoplay: true,
+    //     arrows: false,
+    //     dots: false,
+    //     pauseOnHover: false,
+    //     responsive: [{
+    //         breakpoint: 768,
+    //         settings: {
+    //             slidesToShow: 4
+    //         }
+    //     }, {
+    //         breakpoint: 520,
+    //         settings: {
+    //             slidesToShow: 3
+    //         }
+    //     }]
+    // });
 });
